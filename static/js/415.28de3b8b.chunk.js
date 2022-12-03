1f3f78333aa85f28adc9064c4a396646e6d59e64
mankovskiy <35701366+mankovskiy@users.noreply.github.com>
2022-12-03 18:12:54 +0000

    Deploying to gh-pages from @ mankovskiy/goit-react-hw-08-phonebook@dec9ffe97acbcf251cedcf69d80cbe9fa748f166 🚀

---
+++ static/js/415.28de3b8b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,n){n.r(t),n.d(t,{Home:function(){return o}});var r=n(4190),a=n(3329),o=function(){return(0,a.jsx)(r.M5,{h:"45vw",children:(0,a.jsx)(r.X6,{as:"h1",bgGradient:"linear(to-l, #3a3fdc, #dbee33)",bgClip:"text",fontSize:"6xl",fontWeight:"extrabold",mt:-20,children:"PHONEBOOK"})})}}}]);
+//# sourceMappingURL=415.28de3b8b.chunk.js.map
